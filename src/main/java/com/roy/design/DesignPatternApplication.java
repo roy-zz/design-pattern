95280e8fe39fd95f09885e88d246633fb4fca43e
roy-zz <roystreet91@gmail.com>
2022-01-25 22:13:05 +0900

    Initial Commit

--- src/main/java/com/roy/design/DesignPatternApplication.java
+++ src/main/java/com/roy/design/DesignPatternApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesignPatternApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesignPatternApplication.class, args);
+        SpringApplication.run(DesignPatternApplication.class, args);
 	}
 
 }
